07331a10d05a9566811e8608ec96aa482e49e13d
Gabz129 <gabriellopezcondori@gmail.com>
2015-05-25 21:07:14 -0300

    added player and shot model, added player movement and shots controller

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+    public float speed;
+    Rigidbody shot;
+
+	void Start () {
+        shot = GetComponent<Rigidbody>();
+        shot.velocity = transform.forward * speed;
+	}
+
+}
